f2bc0328bb0152ad80f95ba9f0823272355eaa2f
fhsolutions2025 <teams@fullhousey.com>
2025-08-23 21:35:06 +0530

    summary
    rectified

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
- /// <reference types="next/image-types/global" />
- 
- // NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+/// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/compat/navigation" />
+
+// NOTE: This file should not be edited
+// It ensures Next.js types are picked up by the TypeScript compiler
